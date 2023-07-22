3cf87ba0c3bb601c3e9390f92c1d0c8c0771e8fb
AIpill <swapapis@gmail.com>
2023-07-22 16:20:42 +0100

    Play till get it right and generate new number when you win and want to play again

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Import random libraries
 import random
 
-print("4 chance to get the number right")
 
 # Generate Random number between 1 and 100.
 generatedNumber = random.randint(1, 100)
 
-i = 0
-while i < 4 :
+while True:
 
     # User enter guess number
     userGuessNumber = int(input("Enter your guess number: "))
@@ -18,12 +16,11 @@ while i < 4 :
         if stopPlaying == 'y':
             break
         else:
+            generatedNumber = random.randint(1, 100)
             continue
     elif generatedNumber > userGuessNumber:
         print('Your guess is too low')
     elif generatedNumber < userGuessNumber:
         print('Your guess is too high')
-    
-    i += 1
     
 
